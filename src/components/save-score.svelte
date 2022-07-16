0013d6c3341780ba0b4c85bc99eec36fffa7f125
Tyson910 <Tyson910@lve.missouristate.edu>
2022-07-16 16:21:11 -0400

    Re-create a skeleton of save score component

---
+++ src/components/save-score.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {string} - time it took user to find all matches */
+	export let score;
+</script>
+
+<!-- Write to a DB? -->
+<form>
+	<label
+		>Congrats you finished in
+		<input type="text" id="score" name="score" readOnly value={score} />
+	</label>
+	<label
+		>Please enter a username to save your score
+		<input type="text" id="username" name="username" required placeholder="Username" />
+	</label>
+	<input type="submit" id="submmit" name="submit" class="btn" />
+</form>
